189135a7c3035fbebc56131ecf26b4e27eba688c
Anders Sveen <anders@f12.no>
2024-07-30 11:08:59 +0200

    Just compressed a bit

--- src/main/kotlin/no/mikill/kotlin_htmx/ObjectUtils.kt
+++ src/main/kotlin/no/mikill/kotlin_htmx/ObjectUtils.kt
@@ -18,13 +18,13 @@ inline fun <reified T> resolveProperty(
     instance: Any,
     propertyName: String,
 ): PropertyAndValue<T> {
-    val indexRegexp = "\\[(\\d+)\\]"
     var currentInstance: Any? = instance
     var currentProperty: KProperty1<Any, Any>? = null
 
     propertyName.split(".").forEach { currentPropertyName ->
-        val hasListIndexResult = indexRegexp.toRegex().find(currentPropertyName)
-        val propertyNameNoIndex = indexRegexp.toRegex().replace(currentPropertyName, "")
+        val (listIndexMatchResult, propertyNameNoIndex) = Regex("""\\[(\\d+)\\]""").let {
+            it.find(currentPropertyName) to it.replace(currentPropertyName, "")
+        }
 
         val currentPropertyClass =
             if (currentProperty == null) currentInstance!!::class else currentProperty!!.returnType.classifier as KClass<*>
@@ -34,8 +34,8 @@ inline fun <reified T> resolveProperty(
         currentInstance = if (currentInstance != null) currentProperty?.get(currentInstance!!) else null
 
         // Will have to go deeper if it is a list
-        if (currentInstance is List<*> && hasListIndexResult != null) {
-            currentInstance = (currentInstance as List<*>)[hasListIndexResult.groupValues[1].toInt()]!!
+        if (currentInstance is List<*> && listIndexMatchResult != null) {
+            currentInstance = (currentInstance as List<*>)[listIndexMatchResult.groupValues[1].toInt()]!!
             currentProperty =
                 currentInstance!!::class.memberProperties.find { it.name == propertyNameNoIndex } as KProperty1<Any, Any>?
         }
