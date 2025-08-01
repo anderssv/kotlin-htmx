08abb76ed9ebbaa84bd5c82fedc82c423e59e7a2
Anders Sveen <anders.rene.sveen@bidbax.no>
2025-08-01 09:54:23 +0200

    Removed unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
 
-    implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.156-kotlin-1.5.0")
 
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("com.nfeld.jsonpathkt:jsonpathkt:2.0.1")
